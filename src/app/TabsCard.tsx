cc4384de539f32d03c5c248609a4eae2fd95fb9b
Fay Ash <yaoshiu@qq.com>
2024-11-16 15:17:04 +0800

    feat: add Tabs component and integrate Radix UI Tabs for improved UI navigation

---
+++ src/app/TabsCard.tsx
@@ -0,0 +1,183 @@
+'use client';
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { cn } from '@/lib/utils';
+import {
+  useState,
+  type ButtonHTMLAttributes,
+  type HTMLAttributes,
+} from 'react';
+import { motion } from 'framer-motion';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { ShoppingBag } from 'lucide-react';
+
+const tabs = [
+  {
+    value: 'cleansers',
+    label: 'Cleansers',
+    description: (
+      <p>
+        <i>
+          <strong>Purify</strong>
+        </i>{' '}
+        your skin with our gentle cleanser that effectively removes impurities,
+        providing a <strong>refreshing start</strong> for your skincare routine.
+      </p>
+    ),
+    details:
+      'Our gentle cleanser is formulated to effectively remove makeup, oil, and impurities, ensuring your skin feels clean, refreshed, and prepared for the next steps in your skincare routine.',
+  },
+  {
+    value: 'toners',
+    label: 'Toners',
+    description: (
+      <p>
+        <i>
+          <strong>Balance</strong>
+        </i>{' '}
+        your complexion with our revitalizing toner, helping to tighten pores
+        and leave skin feeling <strong>smooth</strong> and rejuvenated.
+      </p>
+    ),
+    details:
+      'This revitalizing toner helps balance your skin’s natural pH levels while tightening pores and adding a layer of hydration to prepare your skin for better absorption of subsequent products.',
+  },
+  {
+    value: 'exfoliators',
+    label: 'Exfoliators',
+    description: (
+      <p>
+        <i>
+          <strong>Renew</strong>
+        </i>{' '}
+        your skin’s texture with our exfoliator, designed to gently remove dead
+        cells and reveal <strong>smoother</strong>, more vibrant skin.
+      </p>
+    ),
+    details:
+      'Designed to gently slough off dead skin cells, our exfoliator promotes smoother texture, brighter appearance, and a rejuvenated complexion ready for nourishing treatments and hydration.',
+  },
+  {
+    value: 'serums',
+    label: 'Serums',
+    description: (
+      <p>
+        <i>
+          <strong>Nourish</strong>
+        </i>{' '}
+        your skin deeply with our powerful serum, delivering essential
+        ingredients for a <strong>radiant glow</strong> that lasts.
+      </p>
+    ),
+    details:
+      'Our serum is infused with powerful, skin-loving ingredients that penetrate deeply to target signs of aging, dullness, and uneven tone, leaving your skin with a lasting, healthy glow.',
+  },
+  {
+    value: 'moisturizers',
+    label: 'Moisturizers',
+    description: (
+      <p>
+        <i>
+          <strong>Hydrate</strong>
+        </i>{' '}
+        and lock in moisture with our rich moisturizer, ensuring your skin
+        remains <strong>plump</strong> and youthful throughout the day.
+      </p>
+    ),
+    details:
+      'This rich moisturizer deeply hydrates and locks in moisture, creating a protective barrier that keeps your skin feeling soft, supple, and youthful from morning until night.',
+  },
+];
+
+export default function TabsCard() {
+  const [tab, setTab] = useState('cleansers');
+
+  return (
+    <Tabs
+      onValueChange={setTab}
+      defaultValue="cleansers"
+      className="flex gap-20"
+    >
+      <div className="flex flex-col justify-between">
+        <TabsList className="rounded-full p-0 gap-2 w-fit">
+          {tabs.map(({ value, label }) => (
+            <RoundedTabsTrigger key={value} value={value}>
+              <span
+                className={cn(
+                  'z-10 transition-colors font-medium text-sm',
+                  tab === value ? 'delay-150' : '',
+                )}
+              >
+                {label}
+              </span>
+              {tab === value ? (
+                <motion.span
+                  className="absolute inset-0 bg-background rounded-full shadow-sm border"
+                  layoutId="indicator"
+                />
+              ) : null}
+            </RoundedTabsTrigger>
+          ))}
+        </TabsList>
+        {tabs.map(({ value, description }) => (
+          <TabsContent
+            key={value}
+            value={value}
+            className="*:leading-relaxed text-2xl text-muted-foreground [&_strong]:text-foreground [&_strong]:text-3xl"
+          >
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              {description}
+            </motion.div>
+          </TabsContent>
+        ))}
+      </div>
+      {tabs.map(({ value, label, details }) => (
+        <TabsContent key={value} value={value} className="w-min">
+          <motion.p
+            className="text-muted-foreground"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            {details}
+          </motion.p>
+          <Button className="w-64 mt-8 rounded-full" asChild>
+            <Link href={`/${value}`}>
+              <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className="inline-flex justify-between items-center w-full"
+              >
+                Explore Our {label} <ShoppingBag />
+              </motion.span>
+            </Link>
+          </Button>
+        </TabsContent>
+      ))}
+    </Tabs>
+  );
+}
+
+function RoundedTabsTrigger({
+  className,
+  children,
+  ...props
+}: ButtonHTMLAttributes<HTMLButtonElement> & { value: string }) {
+  return (
+    <TabsTrigger
+      className={cn(
+        'relative px-4 rounded-full h-full data-[state=active]:shadow-none data-[state=active]:bg-transparent',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </TabsTrigger>
+  );
+}
